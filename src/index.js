2e2abfabd1b2978b6ff5a793c86a33641fb02b59
JoshDT900 <DJTroutman900@gmail.com>
2022-08-15 10:18:13 -0400

    Add skeleton files and webpack settings

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
+import _ from "lodash";
+import pageLogo from "./pngwing.com.png";
+import './style.css';
+
+const contentElement = document.querySelector('#content')
+
+let addImage = () => {
+  const imgElement = document.createElement('div');
+
+  const myLogo = new Image();
+  myLogo.src = pageLogo;
+
+  imgElement.appendChild(myLogo);
+
+  return imgElement;
+}
+
+contentElement.appendChild(addImage());
+
 console.log("I work now?");
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,26 @@
 const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  // entry: {
+  //   index: './src/index.js',
+  //   print: './src/print.js',
+  // },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    // clean: true,
   },
+  // output: {
+  //   filename: '[name].bundle.js',
+  //   path: path.resolve(__dirname, 'dist'),
+  // },
+  // plugins: [
+  //   new HtmlWebpackPlugin({
+  //     title: 'Output Management',
+  //   }),
+  // ],
   module: {
     rules: [
       {
@@ -18,5 +33,5 @@ module.exports = {
      },
     ]
   },
-  
+ 
 };
